6b9cbdbbeace82f239d5a209bedf61576b9a53c4
simon <simon@130.190.123.244>
2016-05-17 10:23:44 +0200

    Hull can be loaded from config file

--- src/fr/iutvalence/info/dut/m2107/Hull.java
+++ src/fr/iutvalence/info/dut/m2107/Hull.java
@@ -2,8 +2,21 @@ package fr.iutvalence.info.dut.m2107;
 
 public class Hull {
 
-	private int hullId;
+	
 	private Color hullColor;
 	private float protectionLevel;
+	
+	public Hull(Color color, int protection) {
+		this.hullColor=color;
+		this.protectionLevel=protection;
+	}
+	
+	public float getProtectionLevel() {
+		return this.protectionLevel;
+	}
+	public Color getHullColor() {
+		// TODO Auto-generated method stub
+		return this.hullColor;
+	}
 
 }
--- src/fr/iutvalence/info/dut/m2107/Main.java
+++ src/fr/iutvalence/info/dut/m2107/Main.java
@@ -3,9 +3,17 @@ package fr.iutvalence.info.dut.m2107;
 public class Main {
 	public static final String CONFIG_FILE = "Perso.cfg" ;
 	public static void main(String[] args) {
+		/**
+		 * Load weapons
+		 */
 		WeaponSet ws = new WeaponSet();
 		ws.getWeaponFromFile(CONFIG_FILE);
 		System.out.println(ws.toString());
+		
+		HullSet hs = new HullSet();
+		hs.getHullFromFile(CONFIG_FILE);
+		System.out.println(hs.toString());
+		
 		// TODO Auto-generated method stub
 
 	}
