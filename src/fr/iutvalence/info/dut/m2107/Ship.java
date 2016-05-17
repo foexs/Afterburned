9820808bcb770395048469820b9fc80d72579ebc
nico <nico@130.190.123.42>
2016-05-17 10:26:05 +0200

    Added some methods.

--- src/fr/iutvalence/info/dut/m2107/Ship.java
+++ src/fr/iutvalence/info/dut/m2107/Ship.java
@@ -1,15 +1,34 @@
 package fr.iutvalence.info.dut.m2107;
 
+
 public class Ship {
 	
 	//attributes
 	private Weapon currentWeapon;
 	private Hull currentHull;
-	private Hitbox shipPosition;
+	private Dot shipPosition;
 	private int baseDamage;
 	private int healthPoint;
 	private float angle;
 	
+	//shipSize will be used to calculate its hitbox.
+	private int shipSize;
+	private Hitbox shipHitBox;
+	public Ship(Weapon currentWeapon, Hull currentHull,Dot shipPosition , int baseDamage, int healthPoint, float angle, int shipSize, Hitbox shipHitBox)
+	{
+		//Set the default values and constructors
+		//Weapon must have an ID!
+		currentWeapon = new Weapon(null, 1);
+		currentHull = new Hull();
+		shipPosition = new Dot(1,1);
+		baseDamage = 1;		
+		healthPoint = 1;
+		angle = 0;
+		shipSize = 1;
+		shipHitBox = shipSize(shipSize);
+		
+	}
+	//
 	//methods
 	public void Fire() {
 		// TODO - implement Ship.Fire
@@ -18,11 +37,21 @@ public class Ship {
 
 	public void Aim() {
 		// TODO - implement Ship.Aim
+		//Angle 'll be set by the player.
+		this.setAngle(angle);
 		throw new UnsupportedOperationException();
 	}
 
 	public void Move() {
-		// TODO - implement Ship.Move
+		//TODO
+		//if the player wants to go right
+		setPosition(new Dot(this.shipPosition.getX()+1,this.shipPosition.getY()));
+		//if the player wants to go left
+		setPosition(new Dot(this.shipPosition.getX()-1,this.shipPosition.getY()));
+		//if the player wants to go up
+		setPosition(new Dot(this.shipPosition.getX(),this.shipPosition.getY()-1));
+		//if the player wants to go down
+		setPosition(new Dot(this.shipPosition.getX(),this.shipPosition.getY()+1));
 		throw new UnsupportedOperationException();
 	}
 
@@ -30,6 +59,11 @@ public class Ship {
 	
 	//getters and setters
 	
+	public Hitbox shipSize(int sizeMult)
+	{
+		new Hitbox(sizeMult,sizeMult,sizeMult,sizeMult);
+		return shipHitBox;
+	}
 	
 	public Weapon getCurrentWeapon() {
 		return currentWeapon;
@@ -47,13 +81,6 @@ public class Ship {
 		this.currentHull = currentHull;
 	}
 
-	public Hitbox getShipPosition() {
-		return shipPosition;
-	}
-
-	public void setShipPosition(Hitbox shipPosition) {
-		this.shipPosition = shipPosition;
-	}
 
 	public int getBaseDamage() {
 		return baseDamage;
@@ -77,6 +104,11 @@ public class Ship {
 
 	public void setAngle(float angle) {
 		this.angle = angle;
+	}
+	
+	public void setPosition(Dot position)
+	{
+		this.shipPosition=position;
 	}
 
 }
