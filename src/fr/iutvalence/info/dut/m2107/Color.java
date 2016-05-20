5a9e7f27b175f70e64e65d1c4b57c20809f99cdf
reynloui <reynloui@PC-A213-06.iut-valence.fr>
2016-05-20 11:00:39 +0200

    improved color improved perso.cfg

--- src/fr/iutvalence/info/dut/m2107/Color.java
+++ src/fr/iutvalence/info/dut/m2107/Color.java
@@ -1,5 +1,9 @@
 package fr.iutvalence.info.dut.m2107;
-
+/**
+ * Represents the strength of a chosen enemy, weapon or hull.
+ * @author reynloui
+ *
+ */
 public enum Color {
 	red(0xff0000),
 	green(0x00ff00),
@@ -8,11 +12,17 @@ public enum Color {
 	black(0),
 	white(0xffffff);
 	
-	
+	/**
+	 * the color RGB code 
+	 */
 	int colorCode;
 	
 	private Color(int code)
 	{
 		this.colorCode=code;
+	}
+	
+	public int getColorCode(){
+		return this.colorCode;
 	}
 }
