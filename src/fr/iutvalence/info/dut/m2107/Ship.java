ff64488d815b4be8537eafb9cfa4ef43251f690e
simon <simon@Simon-PC-g>
2016-05-20 20:21:29 +0200

    Some little things corrected to avoid errors

--- src/fr/iutvalence/info/dut/m2107/Ship.java
+++ src/fr/iutvalence/info/dut/m2107/Ship.java
@@ -6,26 +6,25 @@ public class Ship {
 	//attributes
 	private Weapon currentWeapon;
 	private Hull currentHull;
-	private Dot shipPosition;
+	private Dot position;
 	private int baseDamage;
 	private int healthPoint;
 	private float angle;
 	
 	//shipSize will be used to calculate its hitbox.
-	private int shipSize;
-	private Hitbox shipHitBox;
+	private int size;
+	//private Hitbox shipHitBox; Removed, direct use of getHitbox instead
 	public Ship(Weapon currentWeapon, Hull currentHull,Dot shipPosition , int baseDamage, int healthPoint, float angle, int shipSize, Hitbox shipHitBox)
 	{
 		//Set the default values and constructors
 		//Weapon must have an ID!
 		this.currentWeapon = currentWeapon;
 		this.currentHull = currentHull;
-		this.shipPosition = shipPosition;
+		this.position = shipPosition;
 		this.baseDamage = baseDamage;		
 		this.healthPoint = healthPoint;
 		this.angle = angle;
-		this.shipSize = shipSize;
-		this.shipHitBox=getHitBox(shipSize);
+		this.size = shipSize;
 	}
 	//
 	//methods
@@ -40,20 +39,27 @@ public class Ship {
 		this.setAngle(angle);
 		throw new UnsupportedOperationException();
 	}
-
+	/**
+	 * Move the ship?
+	 * Might have a parameter to set the direction,
+	 * this one looks not usable for turn based game, but if the game evolve to real time it could be useful
+	 */
 	public void Move() {
 		//TODO
 		//if the player wants to go right
-		setPosition(new Dot(this.shipPosition.getX()+1,this.shipPosition.getY()));
+		setPosition(new Dot(this.position.getX()+1,this.position.getY()));
 		//if the player wants to go left
-		setPosition(new Dot(this.shipPosition.getX()-1,this.shipPosition.getY()));
+		setPosition(new Dot(this.position.getX()-1,this.position.getY()));
 		//if the player wants to go up
-		setPosition(new Dot(this.shipPosition.getX(),this.shipPosition.getY()-1));
+		setPosition(new Dot(this.position.getX(),this.position.getY()-1));
 		//if the player wants to go down
-		setPosition(new Dot(this.shipPosition.getX(),this.shipPosition.getY()+1));
+		setPosition(new Dot(this.position.getX(),this.position.getY()+1));
 		throw new UnsupportedOperationException();
 	}
-
+	
+	public void Move(Dot newCoordinates){
+		this.position=newCoordinates;
+	}
 	
 	
 	//getters and setters
@@ -103,10 +109,12 @@ public class Ship {
 	public void setAngle(float angle) {
 		this.angle = angle;
 	}
-	
 	public void setPosition(Dot position)
 	{
-		this.shipPosition=position;
+		this.position=position;
+	}
+	public Hitbox getHitbox(){
+		return new Hitbox(this.position.getX()-this.size, this.position.getX()+this.size , this.position.getY()-this.size , this.position.getY()+this.size);
 	}
 
 }
