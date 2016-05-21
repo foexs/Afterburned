2d0c3e5d6c267f21c160374c1aaa493c70f1238e
simon <simon@Simon-PC-g>
2016-05-21 12:48:45 +0200

    Slick2D installed, there is the natives for windows, mac and linux x86, displays a 640x480 window with an fps counter, displaying about 800/1000 fps on my computer

---
+++ src/fr/iutvalence/info/dut/m2107/GUI.java
@@ -0,0 +1,41 @@
+package fr.iutvalence.info.dut.m2107;
+
+import org.newdawn.slick.*;
+
+
+/**
+ * Define a gui, to execute you need to have the natives in your build path and this argument:"Djava.library.path=lib/natives"
+ * @author simon
+ *
+ */
+public class GUI extends BasicGame {
+		public GameContainer container;
+	    public GUI() {
+	        super("Lesson 1 :: WindowGame");
+	    }
+
+	    @Override
+	    public void init(GameContainer container) throws SlickException {
+	        this.container = container;
+	    }
+
+	    @Override
+	    public void render(GameContainer container, Graphics g) throws SlickException {
+	    }
+
+	    @Override
+	    public void update(GameContainer container, int delta) throws SlickException {
+	    }
+	    
+	    @Override
+	    public void keyReleased(int key, char c) {
+	        if (Input.KEY_ESCAPE == key) {
+	            container.exit();
+	        }
+	    }
+
+	public static void main(String[] args) throws SlickException {
+		new AppGameContainer(new GUI(), 640, 480, false).start();
+	}
+
+}
