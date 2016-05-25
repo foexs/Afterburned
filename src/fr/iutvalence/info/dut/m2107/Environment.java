077660f72fbeb08c7b143d61a19e5d9ad4404cc6
simon <simon@Simon-PC-g>
2016-05-25 18:30:55 +0200

    Environment updated

--- src/fr/iutvalence/info/dut/m2107/Environment.java
+++ src/fr/iutvalence/info/dut/m2107/Environment.java
@@ -1,11 +1,12 @@
 package fr.iutvalence.info.dut.m2107;
 
 public enum Environment {
-	nebula,
-	asteroidField,
-	sunbelt,
-	orbit,
-	emptySpace;
+	nebula("ihm/environnement brouillard nébuleuse",50,0,0,50),
+	asteroidField("ihm/environnement brouillard nébuleuse",0,50,0,50),
+	sunbelt("ihm/environnement brouillard nébuleuse", 0, 0, 10, 50),
+	orbit("ihm/environnement brouillard nébuleuse", 0, 0, 0, 80),
+	emptySpace("ihm/environnement brouillard nébuleuse", 0, 0, 0, 50);
+	
 	
 	//Nebula
 	int fogLevel;
@@ -16,64 +17,37 @@ public enum Environment {
 	 * sunDamage will set how much damage the player takes. I'll increase as the player gets closer to it.
 	 */
 	int sunDamage;
-	/*
-	 * sunPosition will set where the sunBelt is. I can be on the left side or the right side of the screen.
-	 * Depending of where it is, it'll change how much damage the player takes as he gets closer to its Hitbox.
-	 */
-	Hitbox sunPosition;
+
+	int enemySpawnRate;
 	
-	//Orbit
-	/*
-	 * Depending of where the nearby planet is, the player's shots will be attracted, and so will not have the angle expected by the player.
-	 */
-	int accuracyMalus;
-	/*
-	 * Sets where the beam'll be attracted.
-	 */
-	Hitbox planetPosition;
+	String backgroundPath;
 	
 	//emptySpace
 	//This environment will have no effect on the player.
 	
-	//getters and setters
 	
+	
+	private Environment(String backgroundPath, int fogLevel, int asteroidSpawnRate, int sunDamage, int enemySpawnRate){
+		
+	}
+	//getters
+	
+	public String getBackgroundPath() {
+		return backgroundPath;
+	}
+	public int getEnemySpawnRate() {
+		return enemySpawnRate;
+	}
 	public int getFogLevel() {
 		return fogLevel;
-	}
-	public void setFogLevel(int fogLevel) {
-		this.fogLevel = fogLevel;
 	}
 	public int getAsteroidSpawnRate() {
 		return asteroidSpawnRate;
 	}
-	public void setAsteroidSpawnRate(int asteroidSpawnRate) {
-		this.asteroidSpawnRate = asteroidSpawnRate;
-	}
 	public int getSunDamage() {
 		return sunDamage;
 	}
-	public void setSunDamage(int sunDamage) {
-		this.sunDamage = sunDamage;
-	}
-	public Hitbox getSunPosition() {
-		return sunPosition;
-	}
-	public void setSunPosition(Hitbox sunPosition) {
-		this.sunPosition = sunPosition;
-	}
-	public int getAccuracyMalus() {
-		return accuracyMalus;
-	}
-	public void setAccuracyMalus(int accuracyMalus) {
-		this.accuracyMalus = accuracyMalus;
-	}
-	public Hitbox getPlanetPosition() {
-		return planetPosition;
-	}
-	public void setPlanetPosition(Hitbox planetPosition) {
-		this.planetPosition = planetPosition;
-	}
-	
+
 
 	
 }
